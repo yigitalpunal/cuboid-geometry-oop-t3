392005ac8e7f64aba0ceebb86efa550cc832344d
Yiğit Alp Ünal <62855714+yigitalpunal@users.noreply.github.com>
2025-06-12 00:26:33 +0200

    Initial upload of Cuboid class project

---
+++ Project-3-Yigit-Alp-Unal-OOP.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+using namespace std;
+
+class Cuboid {
+private:
+    int a, b, c; // Edges: length, width, height
+
+public:
+    // Default constructor
+    Cuboid() {
+        a = 0;
+        b = 0;
+        c = 0;
+    }
+
+    // Parameterized constructor
+    Cuboid(int aVal, int bVal, int cVal) {
+        a = aVal;
+        b = bVal;
+        c = cVal;
+    }
+
+    // Set functions
+    void setA(int val) { a = val; }
+    void setB(int val) { b = val; }
+    void setC(int val) { c = val; }
+
+    // Get functions
+    int getA() const { return a; }
+    int getB() const { return b; }
+    int getC() const { return c; }
+
+    // Function to calculate surface area
+    int surfaceArea() const {
+        return 2 * (a * b + a * c + b * c);
+    }
+
+    // Function to calculate volume
+    int volume() const {
+        return a * b * c;
+    }
+
+    // Friend function to input cuboid
+    friend istream& operator>>(istream& in, Cuboid& cub);
+
+    // Friend function to output cuboid
+    friend ostream& operator<<(ostream& out, const Cuboid& cub);
+};
+
+// Input operator
+istream& operator>>(istream& in, Cuboid& cub) {
+    cout << "Enter length (a): ";
+    in >> cub.a;
+    cout << "Enter width (b): ";
+    in >> cub.b;
+    cout << "Enter height (c): ";
+    in >> cub.c;
+    return in;
+}
+
+// Output operator
+ostream& operator<<(ostream& out, const Cuboid& cub) {
+    out << "[" << cub.a << ", " << cub.b << ", " << cub.c << "]";
+    return out;
+}
+
+// Main function
+int main() {
+    Cuboid box1, box2(2, 3, 4);
+
+    cout << "Enter dimensions for box1:\n";
+    cin >> box1;
+
+    cout << "\nBox 1: " << box1 << endl;
+    cout << "Box 2: " << box2 << endl;
+
+    cout << "Surface area of box1: " << box1.surfaceArea() << endl;
+    cout << "Volume of box1: " << box1.volume() << endl;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+## 📁 Task 3 – Cuboid
+
+```markdown
+# Task 3 – Cuboid Class in C++
+
+## Description
+
+This task defines a `Cuboid` class to represent a rectangular 3D box. It includes basic functionality like setting and getting dimensions, calculating volume and surface area, and using `friend` functions for I/O.
+
+### Features:
+
+- Default and parameterized constructors
+- Getter and setter methods for a, b, c (edges)
+- Volume and surface area functions
+- Input (`>>`) and output (`<<`) operators
+- Uses `friend` functions
+
+## Files
+
+- `main.cpp`: Source code for the Cuboid class and example usage
+
+## How to Compile and Run
+
+```bash
+g++ main.cpp -o cuboid
+./cuboid
+
+## Example Output
+
+Enter length (a): 2
+Enter width (b): 3
+Enter height (c): 4
+
+Box 1: [2, 3, 4]
+Box 2: [2, 3, 4]
+Surface area of box1: 52
+Volume of box1: 24
+
+## Author
+
+Yiğit Alp Ünal
+📧 unalyigitalpp@gmail.com
+🌐 github.com/yigitalpunal
